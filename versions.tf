87f757aa81c8da4e430b61e313175425a63d19ac
Hyunjin Staats <hyunjin.staats@adesso.de>
2022-09-02 11:26:13 +0200

    remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,19 +1,19 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "hstaatsxx-learning-terraform"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+  */
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.16"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.2.0"
 }
